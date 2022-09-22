e467b7efa550921053035a7c93bad069dcb85faf
Bryce L Taylor <bryceLTaylor@gmail.com>
2022-09-22 17:07:57 -0400

    Initial setup

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Header from "../Header/Header.js";
+
+import "./App.css";
+
+const App = (props) => (
+  <div className="app">
+    <Header></Header>
+  </div>
+);
+
+export default App;
---
+++ src/components/Header/header.js
@@ -0,0 +1,10 @@
+import react from "react";
+import "./header.css";
+
+const Header = (props) => (
+  <div className="header">
+    <h1>Paper Minis</h1>
+  </div>
+);
+
+export default Header;
