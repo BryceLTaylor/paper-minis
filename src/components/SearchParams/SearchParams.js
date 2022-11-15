72840cd2e9dd7103738b82fc5f9e5d3a64fe2e6d
Bryce L Taylor <bryceLTaylor@gmail.com>
2022-11-14 19:41:03 -0500

    use useState to store search options

--- src/components/SearchParams/SearchParams.js
+++ src/components/SearchParams/SearchParams.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 import "./SearchParams.css";
 
@@ -12,41 +13,80 @@ const SearchParams = (props) => {
     "dragon",
     "monstrosity",
   ];
+  const crOptions = ["equals", "less than", "greater than", "approximately"];
+
+  const [nameSearch, setNameSearch] = useState("");
+  const [crSearch, setCrSearch] = useState("");
+  const [crMatch, setCrMatch] = useState("equals");
+  const [creatureTypeSearch, setCreatureTypeSearch] = useState("any");
+
+  async function getCreatures() {
+    console.log("getting creatures");
+  }
 
   return (
     <div className="search-params">
-      <form className="filters">
-        <label for="creatureName">Creature Name</label>
-        <input id="creatureName"></input>
+      <form className="filters" onSubmit={() => getCreatures()}>
+        <label htmlFor="creatureName">
+          Creature Name
+          <input
+            id="creatureName"
+            onChange={(e) => {
+              setNameSearch(e.target.value);
+            }}
+          ></input>
+        </label>
         <br />
-        <label for="cr">Challenge Rating</label>
-        <input id="cr"></input>
-        <label for="crOptions">CR match?</label>
-        <select id="crOptions">
-          <option>equals</option>
-          <option>less than</option>
-          <option>greater than</option>
-          <option>approximately</option>
-        </select>
+        <label htmlFor="cr">
+          Challenge Rating
+          <input
+            id="cr"
+            onChange={(e) => {
+              setCrSearch(e.target.value);
+            }}
+          ></input>
+        </label>
+        <label htmlFor="crOptions">
+          CR match?
+          <select
+            id="crOptions"
+            onChange={(e) => {
+              setCrMatch(e.target.value);
+            }}
+          >
+            {crOptions.map((option) => (
+              <option key={option}>{option}</option>
+            ))}
+          </select>
+        </label>
         <br />
-        <label for="creatureType">Creature Type</label>
-        <select id="creatureType">
-          {creatureTypes.map((creatureType) => (
-            <option>{creatureType}</option>
-          ))}
-        </select>
-        <button>Submit</button>
+        <label htmlFor="creatureType">
+          Creature Type
+          <select
+            id="creatureType"
+            onChange={(e) => {
+              setCreatureTypeSearch(e.target.value);
+            }}
+          >
+            <option />
+            {creatureTypes.map((creatureType) => (
+              <option key={creatureType}>{creatureType}</option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <button onClick={() => getCreatures()}>Submit</button>
       </form>
 
       <div className="sort-by">
-        <label for="SortFirst">Sort First By:</label>
+        <label htmlFor="SortFirst">Sort First By:</label>
         <select id="sortFirst">
           <option>Name</option>
           <option>Challenge</option>
           <option>Type</option>
         </select>
         <br />
-        <label for="SortSecond">Sort Second By:</label>
+        <label htmlFor="SortSecond">Sort Second By:</label>
         <select id="sortSecond">
           <option>Name</option>
           <option>Challenge</option>
