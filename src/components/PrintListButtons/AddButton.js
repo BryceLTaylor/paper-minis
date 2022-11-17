0cb0c10f1da8822be686bd642c58f51aafcb5e54
Bryce L Taylor <bryceLTaylor@gmail.com>
2022-11-17 13:33:58 -0500

    Pull add button into own component

--- src/components/PrintListButtons/AddButton.js
+++ src/components/PrintListButtons/AddButton.js
@@ -2,32 +2,25 @@ import React from "react";
 import { useState, useContext } from "react";
 
 import printContext from "../../printContext.js";
-import "./PrintListButtons.css";
-import AddButton from "./AddButton.js";
 
-const PrintListButtons = (props) => {
-  const [printList, setPrintList] = useContext(printContext);
-  const [countInList, setCountInList] = useState(0);
+import "./PrintListButtons.css";
+
+const AddButton = (props) => {
+  // props: creatureInfo,  updateListCount
+  [printList, setPrintList] = useContext(printContext);
 
   async function addCreature() {
-    console.log(`add ${props.creatureInfo.name} to printlist`);
+    // console.log(`add ${props.creatureInfo.name} to printlist`);
     let creatureObject = await findCreature(printList, props.creatureInfo.id);
-    console.log(creatureObject);
+    // console.log(creatureObject);
     if (creatureObject === null) {
       addNewCreature();
     } else {
       changeCreatureCount(1);
     }
-    getListCount();
+    props.updateListCount();
   }
 
-  /*
-    {
-        id: #,
-        count: #,
-        creatureInfo: {}
-    }
-  */
   async function findCreature(creatureList, creatureId) {
     let creatureInList = null;
 
@@ -49,9 +42,8 @@ const PrintListButtons = (props) => {
     //add newCreature to copy of the listcontext
     let printListCopy = printList;
     printListCopy.creatures.push(newCreature);
-    console.log(printListCopy);
-    //replace listcontext with copy
-    setPrintList(printListCopy);
+
+    setPrintList(printListCopy); //replace listcontext with copy
   }
 
   async function changeCreatureCount(number) {
@@ -66,43 +58,16 @@ const PrintListButtons = (props) => {
     if (creatureToChange.count <= 0) {
       let index = printListCopy.creatures.indexOf(creatureToChange);
       printListCopy.creatures.splice(index, 1);
-      console.log(printListCopy.creatures);
     }
 
     setPrintList(printListCopy);
   }
 
-  async function getListCount() {
-    let creatureInList = await findCreature(printList, props.creatureInfo.id);
-    // console.log(creatureInList);
-    setCountInList(creatureInList.count);
-  }
-
-  async function removeCreatureFromList() {
-    changeCreatureCount(-1);
-    getListCount();
-  }
-
   return (
-    <div className="print-list-buttons">
-      <AddButton
-        creatureInfo={props.creatureInfo}
-        updateListCount={getListCount}
-      />
-      <button className="add-button" onClick={addCreature}>
-        Add
-      </button>
-      <div>{countInList}</div>
-      {countInList > 0 ? (
-        <button
-          className="remove-button"
-          onClick={() => removeCreatureFromList()}
-        >
-          Remove 1
-        </button>
-      ) : null}
-    </div>
+    <button className="add-button" onClick={addCreature}>
+      Add
+    </button>
   );
 };
 
-export default PrintListButtons;
+export default AddButton;
